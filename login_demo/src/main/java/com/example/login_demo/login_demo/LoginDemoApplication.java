f3f2d6bd4c3e1b2821538bffab6e4399d9111c2d
tama.shima <minatodaisuki2828@gmail.com>
2018-10-07 18:34:20 +0900

    新規作成

---
+++ login_demo/src/main/java/com/example/login_demo/login_demo/LoginDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.login_demo.login_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginDemoApplication.class, args);
+	}
+}
